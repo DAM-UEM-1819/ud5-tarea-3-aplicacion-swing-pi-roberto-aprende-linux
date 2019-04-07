c59bde5b9b0acb3f38833c14d0d2a305585f496a
V1tter <di.viteri.dv@gmail.com>
2019-04-07 19:05:43 +0200

    Informes

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -1,9 +1,20 @@
 package controlador;
 
+import vista.*;
+import vista.Login;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Creacion de objetos
+		Controlador miControlador = new  Controlador();
+		Login login = new Login();
+		Home home = new Home();
+		InformacionExtra infoExtra = new InformacionExtra();
+		
+		
+		
 
 	}
 
