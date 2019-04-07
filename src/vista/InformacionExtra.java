aa75c46f485c7d47aa5571cddc64a850fc3e489b
V1tter <di.viteri.dv@gmail.com>
2019-04-07 13:47:15 +0200

    Gestion acad

--- src/vista/InformacionExtra.java
+++ src/vista/InformacionExtra.java
@@ -35,7 +35,7 @@ public class InformacionExtra extends JFrame {
 
 
 	public InformacionExtra() {
-		setTitle("Actores gesti\u00F3n");
+		setTitle("Información extra");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1000, 800);
 		contentPane = new JPanel();
